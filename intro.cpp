7be2aa45721dba6af3627ccd50292172a72c00df
Iqra <iqraazam4831@gmail.com>
2024-02-21 21:00:25 +0500

    modified

--- intro.cpp
+++ intro.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hello world";
+   int a  , b ;
+   cout<<"enter the value of a";
+   cin>>a;
+   cout<<"enter the value of b";
+   cin>>b;
+   cout<<"\n the sum of a and b is "<<a+b;
+   cout<<"\n the subtraction of a and b is "<<a-b;
+   cout<<"\n the multiplication of a and b is "<<a * b;
+   cout<<"\n the division of a and b is "<<a/b;
+    cout<<"\n the value of a is smaller than  to b "<<(a<b) ;
+   
     return 0;
 }
