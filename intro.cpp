53bfb59667c8c3ac6faa5074933f5816c70873c2
Iqra <iqraazam4831@gmail.com>
2024-02-24 17:56:45 +0500

    modified

--- intro.cpp
+++ intro.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 int main(){
    int a  , b ;
@@ -13,4 +13,84 @@ int main(){
     cout<<"\n the value of a is smaller than  to b "<<(a<b) ;
    
     return 0;
-}
+}*/
+//********Q1***********
+/* #include<iostream>
+ using namespace std;
+ int main(){
+    int i;
+    cout<<"enter a number"; 
+    cin>>i;
+    if(i%2==0){
+        cout<<"the number you entered is an even number"<<endl;
+    }else{
+        cout<<"the number an odd one !";
+    }
+ return 0;
+ }*/
+ //***********Q2**********
+ /*#include<iostream>
+ using namespace std;
+ int main(){
+    int i;
+    cout<<"Enter your percentage plz "<<endl;
+    cin>>i;
+    if(i>=95){
+        cout<<" Congratulations!!you got an A+ "<<endl;
+    }else if(i>=80){
+        cout<<"you got an A,well done! "<<endl;
+    }else if(i>=60){
+        cout<<"you got a B"<<endl;
+    }else{
+        cout<<"you passed it"<<endl;
+    }
+ }*/
+ //**********   Q3*****************
+/*#include<iostream>
+using namespace std;
+int main(){
+    char alpha;
+    cout<<"Enter your fvrt alphabet to know if it's vowel or not"<<endl;
+    cin>>alpha;
+    switch(alpha){
+        case 'a':
+        cout<<"you got a vowel"<<endl;
+        break;
+        case 'e':
+        cout<<"you got a vowel"<<endl;
+        break;
+        case 'i':
+        cout<<"you got a vowel"<<endl;
+        break;
+        case 'o':
+        cout<<"you got a vowel"<<endl;
+        break;
+        case 'u':
+        cout<<"you got a vowel"<<endl;
+        break;
+        default:
+        cout<<"its a consonant";
+        
+
+    }
+    return 0;
+}*/
+/*#include<iostream>
+using namespace std;
+int main(){
+    int x , n;
+    cout<<"Enter your nuber to get its table"<<endl;
+    cin>>n;
+    for(x=1; x<=10;x++){
+        cout<<n;
+        cout<<" * ";
+        cout<<x;
+        cout<<" =  ";
+        cout<<n*x;
+        cout<<endl;
+
+        
+        
+    }
+    return 0;
+}*/
